232225e24a034e15d7f58696409e8e268b701e42
julian <julianig84@hotmailcom>
2023-05-21 13:07:40 -0500

    Actualizao

--- main.py
+++ main.py
@@ -40,40 +40,49 @@ main_frame.place(x=0, y=0, width=600, height=500)
 title_label = tk.Label(main_frame, text="Proyecto estadística", font=("Arial", 20, "bold"), bg='#6377bf', fg='#ffffff')
 title_label.pack(pady=20)
 
-buttons = [
-    ("Ventana 1", create_button_frame(app, "Esta es la ventana 1")),
-    ("Ventana 2", create_button_frame(app, "Esta es la ventana 2")),
-    ("Ventana 3", create_button_frame(app, "Esta es la ventana 3")),
-    ("Ventana 4", create_button_frame(app, "Esta es la ventana 4"))
-]
+# Ventana 1
+window1 = create_button_frame(app, "Esta es la ventana 1")
+window1.place(x=0, y=0, width=600, height=500)
+button1 = tk.Button(main_frame, text="Ventana 1", command=lambda: show_frame(window1), font=("Arial", 12), bg='#4d4d4d',
+                   fg='#ffffff', padx=20, pady=5)
+button1.pack(fill=tk.X, padx=50, pady=10)
 
-for text, frame in buttons:
-    frame.place(x=0, y=0, width=600, height=500)
-    button = tk.Button(main_frame, text=text, command=lambda f=frame: show_frame(f), font=("Arial", 12), bg='#4d4d4d',
-                       fg='#ffffff', padx=20, pady=5)
-    button.pack(fill=tk.X, padx=50, pady=10)
+for i in range(1, 8):
+    btn = tk.Button(window1, text=f"Botón {i}", font=("Arial", 12), bg='#4d4d4d', fg='#ffffff', padx=20, pady=5,
+                    command=lambda p=i: open_page(f"Página {p}"))
+    btn.pack(pady=5)
 
-# Agregar los botones adicionales en cada ventana
-for frame in buttons:
-    for i in range(1, 8):
-        btn = tk.Button(frame[1], text=f"Botón {i}", font=("Arial", 12), bg='#4d4d4d', fg='#ffffff', padx=20, pady=5,
-                        command=lambda p=i: open_page(f"Página {p}"))
-        btn.pack(pady=5)
-
-# Crear ventanas adicionales
+# Ventana 2
 window2 = create_button_frame(app, "Esta es la ventana 2")
+window2.place(x=0, y=0, width=600, height=500)
+button2 = tk.Button(main_frame, text="Ventana 2", command=lambda: show_frame(window2), font=("Arial", 12), bg='#4d4d4d',
+                   fg='#ffffff', padx=20, pady=5)
+button2.pack(fill=tk.X, padx=50, pady=10)
+
 for i in range(1, 8):
     btn = tk.Button(window2, text=f"Botón {i}", font=("Arial", 12), bg='#4d4d4d', fg='#ffffff', padx=20, pady=5,
                     command=lambda p=i: open_page(f"Página {p}"))
     btn.pack(pady=5)
 
+# Ventana 3
 window3 = create_button_frame(app, "Esta es la ventana 3")
+window3.place(x=0, y=0, width=600, height=500)
+button3 = tk.Button(main_frame, text="Ventana 3", command=lambda: show_frame(window3), font=("Arial", 12), bg='#4d4d4d',
+                   fg='#ffffff', padx=20, pady=5)
+button3.pack(fill=tk.X, padx=50, pady=10)
+
 for i in range(1, 8):
     btn = tk.Button(window3, text=f"Botón {i}", font=("Arial", 12), bg='#4d4d4d', fg='#ffffff', padx=20, pady=5,
                     command=lambda p=i: open_page(f"Página {p}"))
     btn.pack(pady=5)
 
+# Ventana 4
 window4 = create_button_frame(app, "Esta es la ventana 4")
+window4.place(x=0, y=0, width=600, height=500)
+button4 = tk.Button(main_frame, text="Ventana 4", command=lambda: show_frame(window4), font=("Arial", 12), bg='#4d4d4d',
+                   fg='#ffffff', padx=20, pady=5)
+button4.pack(fill=tk.X, padx=50, pady=10)
+
 for i in range(1, 8):
     btn = tk.Button(window4, text=f"Botón {i}", font=("Arial", 12), bg='#4d4d4d', fg='#ffffff', padx=20, pady=5,
                     command=lambda p=i: open_page(f"Página {p}"))
